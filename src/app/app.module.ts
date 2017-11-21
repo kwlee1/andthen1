0d66fe306bb24cf6c1ec857f6b4f54d86fd147d0
kwlee <keith.won.lee@gmail.com>
2017-11-20 16:16:29 -0800

    added main children components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,23 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './main/login/login.component';
+import { AboutComponent } from './main/about/about.component';
+import { ShowComponent } from './main/show/show.component';
+import { NewComponent } from './main/new/new.component';
+import { AccountComponent } from './main/account/account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavComponent,
-    MainComponent
+    MainComponent,
+    LoginComponent,
+    AboutComponent,
+    ShowComponent,
+    NewComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
