48ed4ce22ef4219071fb2b042f63e93ec1f26f36
kwlee <keith.won.lee@gmail.com>
2017-11-21 19:27:09 -0800

    some CSS changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { NewComponent } from './main/new/new.component';
 import { ShowComponent } from './main/show/show.component';
+import { AboutComponent } from './main/about/about.component';
 
 const routes: Routes = [
 	{path: "", component: MainComponent, children:[
 		{path: "stories/new", pathMatch: "full", component: NewComponent},
 		{path: "stories/:id", component: ShowComponent}
-	]}
+	]},
+	{path: "about", pathMatch: "full", component:AboutComponent }
 ];
 
 @NgModule({
